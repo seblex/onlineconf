b41eae79de8d22e80ab9d6178f5f9d5d2d7ac808
maxim-konovalov <maksim.konovalov@vk.team>
2023-01-11 19:27:54 +0300

    rm useless things

--- admin/js/src/components/Plugins.tsx
+++ admin/js/src/components/Plugins.tsx
@@ -39,128 +39,25 @@ const styles = (theme: Theme) => createStyles({
 	},
 });
 
-type ServerColumns = 'host' | 'mtime' | 'online' | 'package';
 
 interface ServersProps {
 	onError: (error: unknown) => void;
 }
 
-interface ServersState {
-	servers: API.Server[];
-	orderBy: ServerColumns;
-	order: 'asc' | 'desc';
-	confirmDeleteServer?: API.Server;
-}
 
-class Servers extends React.Component<ServersProps & WithStyles<typeof styles> & WithTranslation, ServersState> {
 
-	state: ServersState = {
-		servers: [],
-		orderBy: 'host',
-		order: 'asc',
-	};
-
-	componentDidMount() {
-		this.load();
-	}
-
-	private async load() {
-		try {
-			const servers = await API.getServers();
-			this.setState(({ orderBy, order }) => this.sort({ servers, orderBy, order }));
-		} catch (error) {
-			this.props.onError(error);
-		}
-	}
-
-	private createSortHandler(column: ServerColumns) {
-		return (event: React.MouseEvent<HTMLElement>) => {
-			this.setState(({ servers, orderBy, order }) => {
-				if (orderBy === column) {
-					order = order === 'asc' ? 'desc' : 'asc';
-				} else {
-					orderBy = column;
-				}
-
-				return this.sort({ servers, orderBy, order });
-			});
-		};
-	}
-
-	private sort({ servers, orderBy, order }: ServersState) {
-		let sort = (a: API.Server, b: API.Server) => smartCompare(a.host, b.host);
-		if (orderBy !== 'host') {
-			const sortByHost = sort;
-			sort = (a, b) => {
-				const av = a[orderBy];
-				const bv = b[orderBy];
-				return av === bv ? sortByHost(a, b) : bv !== null && (av === null || av < bv) ? -1 : 1;
-			};
-		}
-		if (order === 'desc') {
-			const sortAsc = sort;
-			sort = (a, b) => sortAsc(b, a);
-		}
-		servers.sort(sort);
-		return { servers, orderBy, order };
-	}
-
-	private deleteServer(server: API.Server) {
-		if (new Date(server.online) < new Date(Date.now() - 2592000000)) {
-			this.deleteServerNow(server.host);
-		} else {
-			this.setState({ confirmDeleteServer: server });
-		}
-	}
-
-	private async deleteServerNow(host: string) {
-		try {
-			await API.deleteServer(host);
-			this.setState(({ servers }) => ({
-				servers: servers.filter(s => s.host !== host),
-				confirmDeleteServer: undefined,
-			}));
-		} catch (error) {
-			this.props.onError(error);
-		}
-	}
-
-	private renderConfirmDeleteServer() {
-		const server = this.state.confirmDeleteServer;
-		if (server === undefined) {
-			return null;
-		}
-		const handleCloseDialog = () => {
-			this.setState({ confirmDeleteServer: undefined });
-		};
-		const { t } = this.props;
-		return (
-			<Dialog open onClose={handleCloseDialog}>
-				<DialogTitle>{t('server.delete.title')}</DialogTitle>
-				<DialogContent>
-					<DialogContentText>{t('server.delete.message', { server: server.host })}</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button color="primary" onClick={handleCloseDialog}>{t('button.cancel')}</Button>
-					<Button color="primary" onClick={() => this.deleteServerNow(server.host)}>{t('button.ok')}</Button>
-				</DialogActions>
-			</Dialog>
-		);
-	}
-
+class Plugins extends React.Component<ServersProps & WithStyles<typeof styles> & WithTranslation> {
 	static contextType = WhoAmIContext;
 
 	render() {
 		const { classes, t } = this.props;
-		const { servers, orderBy, order } = this.state;
 		return (
 			<div className={classes.root}>
 
-				{this.renderConfirmDeleteServer()}
 			</div>
 		);
 	}
 
 }
 
-export default withTranslation()(withStyles(styles)(Servers));
+export default withTranslation()(withStyles(styles)(Plugins));
